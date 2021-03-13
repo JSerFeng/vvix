fa466fdb2b595a381b8b7a65191753d37a9015d3
FY1010 <1114550440@qq.com>
2021-03-13 13:25:59 +0800

    dts

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-export * from './lib/core'
-export * from './lib/reactivity'
-export * from './lib/renderer'
-export * from './lib/scheduler'
-export * from './lib/vdom'
+export * from './lib'
---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+export * from './core'
+export * from './reactivity'
+export * from './renderer'
+export * from './shared'
+export * from './vdom'
